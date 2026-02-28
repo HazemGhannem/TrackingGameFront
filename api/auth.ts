1aae78773e4f604ef08a70e4143f8e7d449ec609
HazemGhannem <ghannemhazem@gmail.com>
2026-02-28 09:44:50 +0100

    remove token

--- api/auth.ts
+++ api/auth.ts
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { LoginData, SignUpData, User, authResponse } from '../types/api/types';
 import { api } from './axiosInstance';
-const COOKIE_NAME = process.env.NEXT_PUBLIC_COOKIE_NAME;
-const MAX_AGE = 60 * 60 * 24 * 7; // 7 days
 
 export const signupUser = createAsyncThunk<
   authResponse,
@@ -10,10 +8,7 @@ export const signupUser = createAsyncThunk<
   { rejectValue: string }
 >('auth/signup', async (data: SignUpData, { rejectWithValue }) => {
   try {
-    const response = await api.post<authResponse>(
-      ` /api/auth/signup`,
-      data,
-    );
+    const response = await api.post<authResponse>(`/api/auth/signup`, data);
     return response.data;
   } catch (err: any) {
     if (err.response && err.response.data && err.response.data.error) {
@@ -28,12 +23,9 @@ export const loginUser = createAsyncThunk<
   { rejectValue: string }
 >('auth/login', async (data: LoginData, { rejectWithValue }) => {
   try {
-    const response = await api.post<authResponse>(
-      `/api/auth/login`,
-      data,
-    );
+    const response = await api.post<authResponse>(`/api/auth/login`, data);
     const { token, user } = response.data;
-    saveTokenCookie(token);
+    // saveTokenCookie(token);
     return user;
   } catch (err: any) {
     if (err.response && err.response.data && err.response.data.error) {
@@ -42,17 +34,22 @@ export const loginUser = createAsyncThunk<
     return rejectWithValue(err.message || 'login failed');
   }
 });
+// export const logoutUser = createAsyncThunk<void, void>(
+//   'auth/logout',
+//   async () => {
+//     // clearTokenCookie();
+//   },
+// );
 export const logoutUser = createAsyncThunk<void, void>(
   'auth/logout',
   async () => {
-    clearTokenCookie();
+    await api.post('/api/auth/logout'); // ✅ backend clears the cookie
   },
 );
+// function saveTokenCookie(token: string) {
+//   document.cookie = `${COOKIE_NAME}=${token};Max-Age=${MAX_AGE};Path=/;SameSite=None;Secure`;
+// }
 
-function saveTokenCookie(token: string) {
-  document.cookie = `${COOKIE_NAME}=${token};Max-Age=${MAX_AGE};Path=/;SameSite=None;Secure`;
-}
-
-function clearTokenCookie() {
-  document.cookie = `${COOKIE_NAME}=;Max-Age=0;Path=/;SameSite=None;Secure`;
-}
+// function clearTokenCookie() {
+//   document.cookie = `${COOKIE_NAME}=;Max-Age=0;Path=/;SameSite=None;Secure`;
+// }
